8a0d2a1cc8acdbafce5b8b47410b6dece77770fe
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2021-03-14 18:56:07 +0100

    Comment and remove unneded var

--- be I am Online/main.cs
+++ be I am Online/main.cs
@@ -16,37 +16,35 @@ namespace be_I_am_Online
     {
         void iv()
         {
-            this.Invalidate();
+            this.Invalidate();//TRIGGER REPAINT
         }
         public main()
         {
             InitializeComponent();
         }
-        PaintEventArgs eg = null;
-        int wid = 0, height = 0;
+        int wid = 0, height = 0;//VAR FOR WINDOW SIZE
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
             wid = e.ClipRectangle.Width;
             height = e.ClipRectangle.Height;
-            eg = e;
-            paint();
+            paint(e);//START PAINTING
         }
-        public bool connected = false;
-        private void timer1_Tick(object sender, EventArgs e)
+        public bool connected = false;//STORE CONNECTION STATE
+        private void timer1_Tick(object sender, EventArgs e)//TRIGGERED BY TIMER
         {
             try
             {
-                IPHostEntry hostInfo = Dns.GetHostEntry("bing.com");
+                IPHostEntry hostInfo = Dns.GetHostEntry("bing.com");//SEND DNS TO BING
                 connected = true;
             }
             catch (Exception ex)
-            {
+            {//IF DNS FAILED:
                 connected = false;
             }
-            iv();
+            iv();//TRIGGER REPAINT FUNCTION
         }
 
-        void paint()
+        void paint(PaintEventArgs eg)
         {
             if (eg != null)
             {
@@ -56,8 +54,8 @@ namespace be_I_am_Online
                     color = Color.LightGreen;
                 }
                 Brush mb = new SolidBrush(color);
-                eg.Graphics.Clear(Color.White);
-                eg.Graphics.FillEllipse(mb, new Rectangle(new Point(0, 0), new Size(wid, height)));
+                eg.Graphics.Clear(Color.White);//REMOVE OLD PAINTED THINGS AND MAKE THE WINDOW TRANSPARENT
+                eg.Graphics.FillEllipse(mb, new Rectangle(new Point(0, 0), new Size(wid, height)));//PAINT ECLIPSE
                 mb.Dispose();
             }
         }
